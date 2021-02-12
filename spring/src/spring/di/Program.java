9aba45d7c83e48ed6d2bfd6ab488bf0def20081c
kha-github <rlahyuna1234@gmail.com>
2021-02-12 22:15:01 +0900

    setting.xml를 이용한 DI

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,15 +1,20 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		/*
+		/* 
 		Exam exam = new NewlecExam();
 		ExamConsole console = new GridExamConsole();
 		
@@ -18,10 +23,18 @@ public class Program {
 		
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		
 		ExamConsole console = (ExamConsole) context.getBean("console");
 		//ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		List<Exam> exams = (List<Exam>)context.getBean("exams");
+		//exams.add(new NewlecExam(1, 2, 3, 4));
+		
+		for(Exam e : exams)
+			System.out.println(e);
 
 	}
 
