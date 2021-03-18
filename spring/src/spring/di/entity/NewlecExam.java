a2308479f833390a08c1e6ae50f0146b4de84a93
kha-github <rlahyuna1234@gmail.com>
2021-03-18 22:28:13 +0900

    Use javaconfig

--- spring/src/spring/di/entity/NewlecExam.java
+++ spring/src/spring/di/entity/NewlecExam.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-@Controller 
 public class NewlecExam implements Exam {
 
 	@Value("20")
